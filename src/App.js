4b33271b34b45d490b0705912c9ecebaf9ff56a1
00gigs <1onlinegigs@gmail.com>
2024-04-02 14:14:39 -0400

    minor deatails , navbar

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ function App() {
     <div className="min-h-screen">
       <Navbar />
       {/**fill in styled divs with components that make up application */}
-      <div className="bg-[#283739] h-[50vh] w-full">
-        {/**make |hero section| sections with styled div*/}asf
+      <div className="bg-[#283739] h-[70vh] w-full">
+        {/**make |hero section| sections with styled div*/}
       </div>
-      <div className="bg-[#2c5d63] h-[30vh] w-full">{/**make sections with styled div*/}</div>
-      <div className="bg-[#a2c11c] h-[20vh] w-full">{/**make sections with styled div*/}</div>
-      <div className="bg-[#adacac] h-[20vh] w-full">{/**make sections with styled div*/}</div>
+      <div className="bg-[#2c5d63] h-[45vh] w-full">{/**make sections with styled div*/}</div>
+      <div className="bg-[#a2c11c] h-[69vh] w-full">{/**make sections with styled div*/}</div>
+      <div className="bg-[#adacac] h-[50vh] w-full">{/**make sections with styled div*/}</div>
       <div className="bg-[#2c5d63] h-[20vh] w-full">{/**make sections with styled div*/}</div>
     </div>
   );
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,17 +1,27 @@
+'use client'
 import React from 'react'
 import { FaPerson } from "react-icons/fa6";
+import { RiMenu5Line } from "react-icons/ri";
+import { useState } from 'react';
+import { GrClose } from "react-icons/gr";
 const Navbar = () => {
+  const [openMenu, setOpenMenu] = useState(false)
+  const toggle=()=>{
+    setOpenMenu(!openMenu)
+  }
   return (
-    <div className='font-noto-sans text-[34px] bg-[#2540449c] h-[80px]'>
+    <div className='font-noto-sans text-[34px]  bg-[#57c7d89c] h-[80px]'>
       <div className='flex justify-between w-full items-center p-3'>
         <div>
-        <FaPerson className='cursor-pointer h-14 w-11 hover:text-green-200 ml-10'/>
+        <FaPerson className='cursor-pointer h-14 w-11 hover:text-green-600 ml-10'/>
         </div>
-        <div>
-          {/**Main title or welcome message */}
+        <div className='font-rubik font-bold'>
+          MEET JAMES HARRINGTON, The FullStack Engineer👨🏻‍💻
+          {/**Main title or welcome message */} 
         </div>
-        <div>
-          menu
+        <div className='cursor-pointer'>
+        {!openMenu && <RiMenu5Line onClick={toggle} />}
+        {openMenu && <GrClose onClick={toggle} />}
         </div>
       </div>
     </div>
