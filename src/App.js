2bcb235216a4c5e1824bfb0fe7d5a4e1b20db2d2
00gigs <1onlinegigs@gmail.com>
2024-04-02 15:23:04 -0400

    test2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       {/**fill in styled divs with components that make up application */}
       <div className="bg-[#283739] h-[70vh] w-full">
         {/**make |hero section| sections with styled div*/}
-      </div>
+      </div>asfsaf
       <div className="bg-[#2c5d63] h-[45vh] w-full">{/**make sections with styled div*/}</div>
       <div className="bg-[#a2c11c] h-[69vh] w-full">{/**make sections with styled div*/}</div>
       <div className="bg-[#adacac] h-[50vh] w-full">{/**make sections with styled div*/}</div>
