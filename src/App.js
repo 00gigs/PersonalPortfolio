c488847f6fcb290a7ae98823dabff72fdbbc4313
00gigs <1onlinegigs@gmail.com>
2024-04-02 17:20:40 -0400

    hero section day 1

--- src/App.js
+++ src/App.js
@@ -5,13 +5,29 @@ function App() {
     <div className="min-h-screen">
       <Navbar />
       {/**fill in styled divs with components that make up application */}
-      <div className="bg-[#283739] h-[70vh] w-full">
+      <div className="bg-[#28373951] h-[70vh] w-full">
         {/**make |hero section| sections with styled div*/}
-      </div>asfsaf
-      <div className="bg-[#2c5d63] h-[45vh] w-full">{/**make sections with styled div*/}</div>
-      <div className="bg-[#a2c11c] h-[69vh] w-full">{/**make sections with styled div*/}</div>
-      <div className="bg-[#adacac] h-[50vh] w-full">{/**make sections with styled div*/}</div>
-      <div className="bg-[#2c5d63] h-[20vh] w-full">{/**make sections with styled div*/}</div>
+        <div className="grid grid-cols-2">
+          <h1 className=" font-rubik font-bold text-center text-5xl py-10 ">MEET JAMES HARRINGTON, The FullStack Dev👨🏻‍💻</h1>
+          {/**Main title or welcome message */}
+          <p className="flex justify-start ml-12 font-raleway font-thin mt-8">
+            First i would like to thank you for taking the time to  visit my page. You are here to view my skill set and understand why we should engage in business. I want my technology and skills to benefit you and fit your needs.
+        
+          </p>
+        </div>
+      </div>
+      <div className="bg-[#2c5d63] h-[45vh] w-full">
+        {/**make sections with styled div*/}
+      </div>
+      <div className="bg-[#a2c11c] h-[69vh] w-full">
+        {/**make sections with styled div*/}
+      </div>
+      <div className="bg-[#adacac] h-[50vh] w-full">
+        {/**make sections with styled div*/}
+      </div>
+      <div className="bg-[#2c5d63] h-[20vh] w-full">
+        {/**make sections with styled div*/}
+      </div>
     </div>
   );
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -10,15 +10,12 @@ const Navbar = () => {
     setOpenMenu(!openMenu)
   }
   return (
-    <div className='font-noto-sans text-[34px]  bg-[#57c7d89c] h-[80px]'>
+    <div className='font-noto-sans text-[34px] h-[80px]'>
       <div className='flex justify-between w-full items-center p-3'>
         <div>
         <FaPerson className='cursor-pointer h-14 w-11 hover:text-green-600 ml-10'/>
         </div>
-        <div className='font-rubik font-bold'>
-          MEET JAMES HARRINGTON, The FullStack Engineer👨🏻‍💻
-          {/**Main title or welcome message */} 
-        </div>
+      
         <div className='cursor-pointer'>
         {!openMenu && <RiMenu5Line onClick={toggle} />}
         {openMenu && <GrClose onClick={toggle} />}
