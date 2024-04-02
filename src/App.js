2583ad3f438b21c05b9a45e7b7d2df92428a536c
00gigs <1onlinegigs@gmail.com>
2024-04-02 13:51:05 -0400

    color style

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import './App.css';
-import Navbar from './components/navbar';
+import "./App.css";
+import Navbar from "./components/navbar";
 function App() {
   return (
-    <div className="App">
-     <Navbar/>
-     {/**fill in styled divs with components that make up application */}
-     <div>{/**make sections with styled div*/}</div>
-     <div>{/**make sections with styled div*/}</div>
-     <div>{/**make sections with styled div*/}</div>
-     <div>{/**make sections with styled div*/}</div>
-     <div>{/**make sections with styled div*/}</div>
+    <div className="min-h-screen">
+      <Navbar />
+      {/**fill in styled divs with components that make up application */}
+      <div className="bg-[#283739] h-[50vh] w-full">
+        {/**make |hero section| sections with styled div*/}asf
+      </div>
+      <div className="bg-[#2c5d63] h-[30vh] w-full">{/**make sections with styled div*/}</div>
+      <div className="bg-[#a2c11c] h-[20vh] w-full">{/**make sections with styled div*/}</div>
+      <div className="bg-[#adacac] h-[20vh] w-full">{/**make sections with styled div*/}</div>
+      <div className="bg-[#2c5d63] h-[20vh] w-full">{/**make sections with styled div*/}</div>
     </div>
   );
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import { FaPerson } from "react-icons/fa6";
 const Navbar = () => {
   return (
-    <div className=''>
-        Navbar
+    <div className='font-noto-sans text-[34px] bg-[#2540449c] h-[80px]'>
+      <div className='flex justify-between w-full items-center p-3'>
+        <div>
+        <FaPerson className='cursor-pointer h-14 w-11 hover:text-green-200 ml-10'/>
+        </div>
+        <div>
+          {/**Main title or welcome message */}
+        </div>
+        <div>
+          menu
+        </div>
+      </div>
     </div>
   )
 }
