a7cad0c6402fc6d25055a948d2d83684442d43bd
00gigs <1onlinegigs@gmail.com>
2024-04-04 13:15:10 -0400

    updated details of technology section

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,58 +1,142 @@
-import React from 'react'
-import { SiJavascript } from "react-icons/si";
-import { FaNode } from "react-icons/fa";
-import { FaJava } from "react-icons/fa";
-import { FaPython } from "react-icons/fa6";
-import { FaReact } from "react-icons/fa6";
+import React from "react";
+import { FaJava, FaNode,FaWordpressSimple} from "react-icons/fa";
+import { FaReact,FaPython } from "react-icons/fa6";
 import { TbBrandNextjs } from "react-icons/tb";
 import { BiLogoPostgresql } from "react-icons/bi";
-import { SiMongodb } from "react-icons/si";
+import { SiMongodb,SiJavascript,SiExpress,SiCsharp,SiTypescript} from "react-icons/si";
 import { GrMysql } from "react-icons/gr";
-import { FaWordpressSimple } from "react-icons/fa";
-import { SiTypescript } from "react-icons/si";
 import { LiaPhp } from "react-icons/lia";
-import { SiCsharp } from "react-icons/si";
 import { DiDjango } from "react-icons/di";
-import { SiExpress } from "react-icons/si";
-{/**tech stacks */}
 const Technologies = () => {
   return (
-    <div >
-        <div className='font-roboto font-bold text-4xl moving-text tracking-widest my-10 shadow-2xl bg-slate-300 p-2 rounded-xl'>TECHNOLOGY TOOLBOX 🧰</div>
-        <div className='grid  grid-rows-6 grid-flow-col  gap-9  ml-[2rem]'>
-        <FaReact className='text-[3rem]' />
-        <p className='text-center'>get ya lor p tag yea mean</p>
-        <TbBrandNextjs className='text-[3rem]' />
-        <p className='text-center'>get ya lor p tag yea mean</p>
-        <DiDjango className='text-[3rem]' />
-        <p className='text-center'>get ya lor p tag yea mean</p>
-        <FaWordpressSimple className='text-[3rem]' />
-        <p className='text-center'>get ya lor p tag yea mean</p>
-      <SiJavascript className='text-[3rem]' />
-      <p className='text-center'>get ya lor p tag yea mean</p>
-        <SiTypescript className='text-[3rem]' />
-        <p className='text-center'>get ya lor p tag yea mean</p>
-         <FaPython className='text-[3rem]' />
-         <p className='text-center'>get ya lor p tag yea mean</p>
-        <LiaPhp className='text-[3rem]' />
-        <p className='text-center'>get ya lor p tag yea mean</p>
-        <FaJava className='text-[3rem]' />
-        <p className='text-center'>get ya lor p tag yea mean</p>
-        <SiCsharp className='text-[3rem]'/>
-        <p className='text-center'>get ya lor p tag yea mean</p>
-        <BiLogoPostgresql className='text-[3rem]' />
-        <p className='text-center'>get ya lor p tag yea mean</p>
-        <SiMongodb className='text-[3rem]' />
-        <p className='text-center'>get ya lor p tag yea mean</p>
-        <GrMysql className='text-[3rem]' />
-        <p className='text-center'>get ya lor p tag yea mean</p>
-        <FaNode className='text-[3rem]' />
-        <p className='text-center'>get ya lor p tag yea mean</p>
-        <SiExpress className='text-[3rem]'/>
-        <p className='text-center'>get ya lor p tag yea mean</p>
+    <div>
+      <div className="font-roboto font-bold text-4xl moving-text tracking-widest  shadow-2xl bg-slate-300 p-1 rounded-xl">
+        TECHNOLOGY TOOLBOX 🧰
+      </div>
+      <div className="grid  grid-rows-3 grid-flow-col  mt-14 ml-[2rem] gap-5">
+        <div>
+          <FaReact className="text-[3rem] bg-[#2fa2cc] rounded-xl" />
+          <p className=" font-roboto mt-3 ">
+            Enables interactive user interfaces with reusable components,
+            virtual DOM for efficient updates, and a vast ecosystem for web app
+            development.
+          </p>
         </div>
+        <div>
+          <TbBrandNextjs className="text-[3rem] bg-slate-200 rounded-xl" />
+          <p className=" font-roboto mt-2">
+            Optimizes React apps with server-side rendering, static site
+            generation, and automatic code splitting, enhancing performance and
+            SEO.
+          </p>
+        </div>
+        <div>
+          <DiDjango className="text-[3rem] bg-green-500 rounded-xl" />
+          <p className=" font-roboto">
+            {" "}
+            Comprehensive Python framework offers rapid development, a
+            built-in admin panel, and robust security features for complex web
+            applications.
+          </p>
+        </div>
+        <div>
+          <FaWordpressSimple className="text-[3rem] bg-slate-200 rounded-xl" />
+          <p className=" font-roboto mt-2">
+            Flexible content management system for creating and managing
+            client content  with ease, featuring thousands of themes and plugins for
+            customization.
+          </p>
+        </div>
+        <div>
+          <SiJavascript className="text-[3rem] bg-yellow-300 rounded-xl" />
+          <p className=" font-roboto mt-2">
+            {" "}
+            An essential language for web development, enabling interactive web
+            content, client-side scripting, used as the backbone of modern web
+            applications.
+          </p>
+        </div>
+        <div>
+          <SiTypescript className="text-[3rem] bg-blue-300 rounded-xl" />
+          <p className=" font-roboto mt-2">
+            Enhances JavaScript with static typing, facilitating code
+            reliability and maintainability, ideally used for large-scale application
+            development.
+          </p>
+        </div>
+        <div>
+          <FaPython className="text-[3rem] bg-gradient-to-r from-blue-300 to-yellow-300  rounded-xl" />
+          <p className=" font-roboto mt-2">
+            Python: Used for data driven web development, data analysis, AI, and
+            scientific computing, praised for its readability and extensive
+            libraries.
+          </p>
+        </div>
+        <div>
+          <LiaPhp className="text-[3rem] bg-purple-500 rounded-xl" />
+          <p className=" font-roboto mt-2">
+            Powers dynamic web content and applications,  used for
+            server-side scripting, database integration, and content management
+            systems.
+          </p>
+        </div>
+        <div>
+          <FaJava className="text-[3rem] bg-gradient-to-r from-red-600 to-blue-300  rounded-xl" />
+          <p className=" font-roboto mt-2">
+            Platform-independent, object-oriented programming language used for
+            its portability, security features, and  application at enterprise scale.
+          </p>
+        </div>
+        <div>
+          <SiCsharp className="text-[3rem] bg-blue-500 rounded-xl" />
+          <p className=" font-roboto mt-2">
+            {" "}
+            Allows control over system resources and performance, suited for
+            system/software development, game programming, and real-time
+            applications.
+          </p>
+        </div>
+        <div>
+          <BiLogoPostgresql className="text-[3rem] bg-blue-200 rounded-xl" />
+          <p className=" font-roboto mt-2">
+            Feature-rich SQL database allowing advanced data types, full-text
+            search, and ACID compliance for reliable transactional processing.
+          </p>
+        </div>
+        <div>
+          <SiMongodb className="text-[3rem] bg-green-700 rounded-xl" />
+          <p className=" font-roboto mt-2">
+            NoSQL database  for implementing scalability and flexibility with a
+            document model, suitable for applications needing rapid development
+            and horizontal scaling.
+          </p>
+        </div>
+        <div>
+          <GrMysql className="text-[3rem] bg-gradient-to-r from-blue-300 to-[#ef7529] rounded-xl" />
+          <p className=" font-roboto mt-2">
+            Relational database system, using  reliability and
+            efficiency for web applications and supporting a range of
+            programming languages.
+          </p>
+        </div>
+        <div>
+          <FaNode className="text-[3rem] bg-[#15a03b] rounded-xl" />
+          <p className=" font-roboto mt-2">
+            Event-driven JavaScript runtime environment, allowing scalable
+            server-side and networking applications, popular for its
+            non-blocking I/O model.
+          </p>
+        </div>
+        <div>
+          <SiExpress className="text-[3rem] bg-slate-200 rounded-xl" />
+          <p className=" font-roboto mt-2">
+            Web application framework for Node.js, simplifies the development
+            of web applications and RESTful APIs with minimal overhead.
+          </p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Technologies
+export default Technologies;
