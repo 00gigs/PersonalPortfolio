ed4ef61ab8babcd35c635363bbafa016ccca70f7
00gigs <1onlinegigs@gmail.com>
2024-04-06 13:41:03 -0400

    added nodemailer feature for contact forum

---
+++ BE/server.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+const cors = require('cors');
+const express = require('express')
+const nodemailer = require('nodemailer');
+const app = express()
+const port = 3001
+app.use(cors())
+app.use(express.json())
+
+
+const transporter = nodemailer.createTransport({
+    service:'gmail',
+    auth:{
+        user:'1onlinegigs@gmail.com',
+        pass:process.env.EMAIL_PASSWORD,
+    }
+})
+
+
+
+app.post('/send-email',(req,res)=>{
+    const {name,email,phone} = req.body
+    const mailOptions = {
+        from:'1onlinegigs@gmail.com',
+        to:'jjharrington.business@gmail.com',
+        subject:'Portfolio Site Lead',
+        text:`name:${name},email:${email},phone:${phone}`,
+    }
+
+    transporter.sendMail(mailOptions,(error,info)=>{
+        if(error){
+            console.log(error)
+            res.status(500).json({error:'error sending email'})
+        }else{
+            console.log('Email sent: ' + info.response);
+      res.status(200).json({success:'Email sent '})
+        }
+    })
+})
+
+
+app.listen(port,()=>{
+    console.log(`Server running at http://localhost:${port}`);
+})
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,34 @@
 import React from 'react'
-
+import axios from 'axios'
 const Contact = () => {
 
   const email = (e)=>{
     e.preventDefault()
-    console.log('email')
-  }
+const forumData = new FormData(e.target)
+
+const data = Object.fromEntries(forumData.entries())
+    
+axios.post('http://localhost:3001/send-email',data)
+  .then((response)=>{
+  console.log('Success:', response.data);
+})
+  .catch((error)=>{
+console.error('Error',error)
+})
+
+};
+
+
+
   return (
     <div className='mt-14 flex justify-center '>
         <div className='bg-white h-[40em] w-[40em] rounded-xl shadow-xl flex justify-center items-center'>
-            <form className='flex flex-col gap-10'>
-                <p className='font-roboto'>If you made it this far , we can go further.</p>
-            <input className='border-b' placeholder='Name' name='' type='text'/>
-            <input className='border-b' placeholder='Email' name='' type='email'/>
-            <input className='border-b' placeholder='Phone' name='' type='text'/>
-            <button onClick={email} className='border-2 border-black rounded-lg bg-[#2c5d63c3] hover:bg-[#3d7177b1]'>Contact</button>
+            <form onSubmit={email} className='flex flex-col gap-10'>
+                <p className='font-Roboto'>If you made it this far , we can go further.</p>
+            <input className='border-b' placeholder='Name' name='name' type='text'/>
+            <input className='border-b' placeholder='Email' name='email' type='email'/>
+            <input className='border-b' placeholder='Phone' name='phone' type='text'/>
+            <button type='submit' className='border-2 border-black rounded-lg bg-[#2c5d63c3] hover:bg-[#3d7177b1]'>Contact</button>
             </form>
         </div>
     </div>
