677f4f915717e84ff4d735f4b021fd76c2b823f1
00gigs <1onlinegigs@gmail.com>
2024-04-06 19:51:32 -0400

    server express changes

--- BE/server.js
+++ BE/server.js
@@ -1,12 +1,21 @@
 require('dotenv').config();
 const cors = require('cors');
 const express = require('express')
+const path = require('path');
 const nodemailer = require('nodemailer');
 const app = express()
 const port = process.env.PORT || 3001
-app.get("/https://personalportfolio-dcjm.onrender.com", (req, res) => res.type('html').send(html));
 app.use(cors())
 app.use(express.json())
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '..', '..', 'build')));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', '..', 'build', 'index.html'));
+});
 
 
 const transporter = nodemailer.createTransport({
